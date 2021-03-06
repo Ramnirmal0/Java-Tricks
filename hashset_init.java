ed4507ebc782df0ffc8706ee77fe7c63f7a48ec3
Nirmal Ram <36753701+Ramnirmal0@users.noreply.github.com>
2021-03-07 04:33:41 +0530

    Update hashset_init.java

--- hashset_init.java
+++ hashset_init.java
@@ -10,3 +10,27 @@ public class Main
 		 System.out.print(hs);
 	}
 }
+
+/*  integer hasset and it's printing*/ 
+
+import java.io.*;
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    int a[] = {1 , 3 , 2 , 2 , 3 , 3 };
+	    Set<Integer> hs = new HashSet<Integer>();
+	    
+	    for(int i=0; i <a.length ; i++){
+	        hs.add(a[i]);
+	    }
+	    
+	    Iterator<Integer> it = hs.iterator();
+	    while(it.hasNext()){
+	        System.out.print(it.next()+" ");
+	    }
+	    
+		
+	}
+	
